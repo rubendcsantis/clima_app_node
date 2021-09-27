0da2b4c564d1f9d31254bac31e8a42d0a3078c4e
RubenSantis <ruben.cespedes@imexhs.com>
2021-09-27 18:13:20 -0500

    Done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # clima_app_node
+    axios": "^0.21.4",
+    "colors": "^1.4.0",
+    "dotenv": "^10.0.0",
+    "inquirer": "^8.1.5"
---
+++ helpers/inquirer.js
@@ -0,0 +1,113 @@
+const inquirer = require('inquirer');
+require('colors');
+
+const preguntas = [
+    {
+        type: 'list',
+        name: 'opcion',
+        message: '¿Qué desea hacer?',
+        choices: [
+            {
+                value: 1,
+                name: `${'1.'.green} Buscar ciudad`
+            },
+            {
+                value: 2,
+                name: `${'2.'.green} Historial`
+            },
+            {
+                value: 0,
+                name: `${'0.'.green} Salir`
+            }
+        ]
+    }
+]
+
+const inquirerMenu = async () => {
+
+    console.clear();
+    console.log('============================'.green);
+    console.log('   Seleccione una opción'.white);
+    console.log('============================\n'.green);
+
+    const { opcion } = await inquirer.prompt(preguntas)
+
+    return opcion;
+}
+
+const pausa = async () => {
+
+    const question = [
+        {
+            type: 'input',
+            name: 'preguntaPausa',
+            message: `Presione ${'ENTER'.green} para continuar.\n`
+        }
+    ]
+
+    await inquirer.prompt(question)
+}
+
+const leerInput = async (message) => {
+
+    const quiestion = [
+        {
+            type: 'input',
+            name: 'desc',
+            message,
+            validate(value) {
+                if (value.length === 0) {
+                    return 'Por favor ingrese un valor';
+                }
+                return true;
+            }
+        }
+    ];
+
+    const { desc } = await inquirer.prompt(quiestion);
+    return desc;
+}
+
+const listadoLugares = async (lugares = []) => {
+
+    const choices = lugares.map((lugar, i) => {
+        const idx = `${i + 1}.`.green;
+        return {
+            value: lugar.id,
+            name: `${idx} ${lugar.nombre}`
+        }
+    });
+
+    choices.unshift({
+        value: '0',
+        name: '0.'.green + ' Cancelar'
+    })
+
+    const preguntas = [{
+        type: 'list',
+        name: 'id',
+        message: 'Seleccione',
+        choices
+    }]
+    const { id } = await inquirer.prompt(preguntas)
+    return id;
+}
+
+const confirmar = async (message) => {
+    const quiestion = {
+        type: 'confirm',
+        name: 'ok',
+        message
+    };
+
+    const { ok } = await inquirer.prompt(quiestion);
+    return ok;
+}
+
+module.exports = {
+    inquirerMenu,
+    pausa,
+    leerInput,
+    listadoLugares,
+    confirmar
+}
